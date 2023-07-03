f18df9d1c17202ea7c3602e64330e0e424eaa7c9
Raphael Kauan <raphaelkauanoficial@gmail.com>
2023-07-03 17:39:42 -0400

    destructuring objects and arrays

---
+++ JS III - JS Moderno/6. Desestruturação de objetos e arrays/index.js
@@ -0,0 +1,33 @@
+// Cria um obj pessoa
+const pessoa = {
+    nome: 'raphael',
+    idade: 19,
+    parentes: ['natan', 'kemilly']
+}
+
+// Maneira antiga
+console.log(pessoa.nome)
+
+// Desestruturação de objetos
+const { idade, parentes } = pessoa
+console.log(idade, parentes)
+
+// Desestruturação de arrays
+const [primeiro, segundo] = parentes
+console.log(primeiro, segundo)
+
+// Desestruturação de funções
+
+function user({nome, idade, parentes}) {
+    // Gera número aleatório
+    const id = Math.floor(Math.random() * 9999)
+    return {
+        id,
+        nome, 
+        idade, 
+        parentes
+    }
+}
+
+const alguem = user(pessoa)
+console.log(alguem)
